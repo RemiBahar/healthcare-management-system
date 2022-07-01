514bd4ad7f980b81c31fe5fcc2c480722fd81b43
RemiBahar <remi.bahar.184.rb@gmail.com>
2022-07-01 13:30:42 +0100

    javadoc

---
+++ api/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.cmd.hms.patient"},{"l":"com.cmd.hms.patient.config"},{"l":"com.cmd.hms.patient.model"},{"l":"com.cmd.hms.patient.service"}];updateSearchResults();
